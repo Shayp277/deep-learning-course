2bdb2bdc69fd32d64016c87215ed519d15f1ead0
shayp <Shayp277@gmail.com>
2025-07-03 02:39:10 +0300

    updated test file to work with custom data loader

--- Util/test.py
+++ Util/test.py
@@ -3,6 +3,8 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 from model.CNN_classifier import*
 from Util.data_preprocessing import *
+from Util.train import *
+from Util.grid_search import *
 from torch.utils.tensorboard import SummaryWriter
 
 
@@ -44,16 +46,25 @@ def evaluate_model_on_test(test_loader, model_path, device):
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+    root = "../data"
+    audio_paths, labels, label_map = load_audio_paths_and_labels(root)
+    train_idx, val_idx, test_idx = create_split_masks(get_data_size(root), train_ratio=0.7, val_ratio=0.2,
+                                                         test_ratio=0.1, seed=0)
+    testing_paths = [audio_paths[i] for i in test_idx]
+    test_labels = [labels[i] for i in test_idx]
+    test_loader = DataLoader(AugmentedMFCCDataset(testing_paths, test_labels, label_map, training=False), batch_size=1, shuffle=False)
+
+
     # test model without mixup
     print('Model score without mix up')
-    best_model_dir = 'best_model'
+    best_model_dir = 'shay_best_model'
     data = DATA('../data', wav_to_pkl=False, with_mixup=False)
-    evaluate_model_on_test(data.X_test, data.y_test, '../' + best_model_dir, device)
-    evaluate_model_on_test(data.X_test_with_mixup, data.y_test_with_mixup, '../' + best_model_dir, device)
+    evaluate_model_on_test(test_loader, '../' + best_model_dir, device)
+    evaluate_model_on_test(test_loader, '../' + best_model_dir, device)
 
     # test model with mixup
-    print('\nModel score with mix up')
-    best_model_dir = 'best_model_with_mixup'
-    data = DATA('../data', wav_to_pkl=False, with_mixup=True)
-    evaluate_model_on_test(data.X_test, data.y_test, '../' + best_model_dir, device)
-    evaluate_model_on_test(data.X_test_with_mixup, data.y_test_with_mixup, '../' + best_model_dir, device)
+    # print('\nModel score with mix up')
+    # best_model_dir = 'best_model_with_mixup'
+    # data = DATA('../data', wav_to_pkl=False, with_mixup=True)
+    # evaluate_model_on_test(test_loader, '../' + best_model_dir, device)
+    # evaluate_model_on_test(test_loader, '../' + best_model_dir, device)
